b49d28ba5b9b74c26ed4351e0fca33c1633636c9
Laavanya-Agarwal <71713140+Laavanya-Agarwal@users.noreply.github.com>
2021-09-01 17:10:32 +0530

    Update caculatingInterest.py

--- Automate - C6/caculatingInterest.py
+++ Automate - C6/caculatingInterest.py
@@ -1,4 +1,5 @@
 # formula for calculating the interests
+print('')
 print('These are the formulas for calculating the interests')
 print('Simple Interest = P × R × N where P=Principal amount R=Annual interest rate N=Term of loan in years')
 print('Compound Interest = P [(1 + R)T – 1] where P=Principal amount R=Annual interest rate T=Number of years interest is applied')
@@ -12,14 +13,14 @@ time = input('Enter the number of years of loan: ')
 print('')
 
 # simple interest
-numerator = str(principle*rate*time)
+numerator = principle*rate*time
 simple = numerator/100
 print('Simple Interest = ', simple)
 print('')
 
 # compound interest
-pt1 = str((1 + rate)*time - 1)
+pt1 = (1 + rate)*time - 1
 pt1 -= 1
-pt2 = str(principle*pt1)
+pt2 = principle*pt1
 print('Compound Interest = ', pt2)
 print('')
